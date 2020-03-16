21c415da3dfe50890ed8e025763df171abaa99b6
Jacques Lefebvre <jacques@les-tilleuls.coop>
2020-03-16 15:38:07 +0100

    Add CsaGuzzle and ChuckNorris

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Csa\Bundle\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
 ];
